e503097d0b9b703aa6ead6ccc4cf8a602aa2f022
David Moser <davmoser@amazon.com>
2020-06-01 22:52:59 -0700

    add settings file for config options

--- mobile/index.js
+++ mobile/index.js
@@ -5,6 +5,10 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 console.disableYellowBox = true;
 
--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,44 +1,27 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { withAuthenticator } from 'aws-amplify-react-native'
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
-Amplify.configure(awsconfig);
+import settings from './settings.json';
 
-import React from 'react';
-import {
-  StyleSheet,
-} from 'react-native';
-
-//TODO: put access token in a settings.json file
-MapboxGL.setAccessToken("");
-
+MapboxGL.setAccessToken(settings.mapboxApiAccessToken);
 
 const App: React.FC = () => {
+  
   return (
 
-      <MapboxGL.MapView style={styles.map} >
-        <MapboxGL.Camera
-            zoomLevel={9}
-            centerCoordinate={[-122.185435, 37.666743]}
-          />
-        </MapboxGL.MapView>
+    <MapboxGL.MapView style={styles.map} >
+      <MapboxGL.Camera
+          zoomLevel={9}
+          centerCoordinate={[-122.185435, 37.666743]}
+        />
+      </MapboxGL.MapView>
 
   );
 };
 
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  },
-  container: {
-    height: 300,
-    width: 300,
-    backgroundColor: "tomato"
-  },
   map: {
     flex: 1
   }
